d84d981fd93d950f329fa35c3fc199e4643bd8df
nobe4 <nobe4@users.noreply.github.com>
2025-02-02 11:15:04 +0100

    feat: add reading of the config file into a yaml struct

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const configPath = core.getInput("config-path", { required: true });
+  const config = yaml.load(fs.readFileSync(configPath, "utf8"));
+  console.log(config);
 } catch (error) {
   core.setFailed(error.message);
 }
