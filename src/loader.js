e00d101d819adb13f91e043fcc04445e66bad09c
nobe4 <nobe4@users.noreply.github.com>
2025-02-04 23:47:56 +0100

    feat: add more logs

--- src/loader.js
+++ src/loader.js
@@ -1,15 +1,29 @@
+const core = require("@actions/core");
+
 async function fetchAll(octokit, config) {
 	const promises = [];
 
 	for (let l of config.links) {
-		promises.push(fetch(octokit, l.from).then((c) => (l.from.content = c)));
-		promises.push(fetch(octokit, l.to).then((c) => (l.to.content = c)));
+		promises.push(
+			fetch(octokit, l.from).then((c) => {
+				core.debug(`fetched ${l.from.path}: ${c}`);
+				l.from.content = c;
+			}),
+		);
+		promises.push(
+			fetch(octokit, l.to).then((c) => {
+				core.debug(`fetched ${l.to.path}: ${c}`);
+				l.to.content = c;
+			}),
+		);
 	}
 
 	return Promise.all(promises);
 }
 
 async function fetch(octokit, { repo: { owner, name }, path }) {
+	core.debug(`fetching ${owner}/${name}:${path}`);
+
 	octokit.rest.repos.getContent({
 		owner: owner,
 		repo: name,
