25ad50092fbd5b056d87b8420fa667ef32d35d7b
nobe4 <nobe4@users.noreply.github.com>
2025-02-04 23:32:17 +0100

    feat: add content fetcher

---
+++ src/loader.js
@@ -0,0 +1,20 @@
+async function fetchAll(octokit, config) {
+	const promises = [];
+
+	for (let l of config) {
+		promises.push(fetch(octokit, l.from).then((c) => (l.from.content = c)));
+		promises.push(fetch(octokit, l.to).then((c) => (l.to.content = c)));
+	}
+
+	return Promise.all(promises);
+}
+
+async function fetch(octokit, { repo: { owner, name }, path }) {
+	octokit.rest.repos.getContent({
+		owner: owner,
+		repo: name,
+		path: path,
+	});
+}
+
+module.exports = { fetchAll };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 const config = require("./config");
+const { fetchAll } = require("./loader");
 
 try {
 	const configPath = core.getInput("config-path", { required: true });
+	let token = core.getInput("token", { required: false });
+
+	if (token === "") {
+		core.debug("Input `token` is empty, using default GITHUB_TOKEN.");
+		token = github.token;
+	}
+
+	const octokit = github.getOctokit(token);
 
 	config
 		.load(configPath)
 		.then((c) => {
 			core.info(`config: ${JSON.stringify(c)}`);
+			return fetchAll(octokit, c);
+		})
+		.then((c) => {
+			core.info(`config after fetch: ${JSON.stringify(c)}`);
 		})
 		.catch((e) => {
 			core.setFailed(e);
