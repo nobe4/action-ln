e43f9b7b7a271dd824fe11bba1139700c7f914e8
nobe4 <nobe4@users.noreply.github.com>
2025-02-05 00:27:29 +0100

    feat: update the config in a different way

--- src/loader.js
+++ src/loader.js
@@ -3,25 +3,44 @@ const core = require("@actions/core");
 async function fetchAll(octokit, config) {
 	const promises = [];
 
-	for (let l of config.links) {
+	//for (let l of config.links) {
+	//	promises.push(
+	//		fetch(octokit, l.from).then((c) => {
+	//			core.debug(`fetched ${JSON.stringify(l.from)}: ${JSON.stringify(c)}`);
+	//			l.from.content = c;
+	//			return l.from;
+	//		}),
+	//	);
+	//	promises.push(
+	//		fetch(octokit, l.to).then((c) => {
+	//			core.debug(`fetched ${JSON.stringify(l.to)}: ${JSON.stringify(c)}`);
+	//			l.to.content = c;
+	//
+	//			return l.to;
+	//		}),
+	//	);
+	//}
+
+	for (let i in config.links) {
 		promises.push(
-			fetch(octokit, l.from).then((c) => {
-				core.debug(`fetched ${JSON.stringify(l.from)}: ${JSON.stringify(c)}`);
-				l.from.content = c;
-				return l.from;
+			fetch(octokit, config.links[i].from).then((c) => {
+				core.debug(
+					`fetched ${JSON.stringify(config.links[i].from)}: ${JSON.stringify(c)}`,
+				);
+				config.links[i].from.content = c;
 			}),
 		);
 		promises.push(
-			fetch(octokit, l.to).then((c) => {
-				core.debug(`fetched ${JSON.stringify(l.to)}: ${JSON.stringify(c)}`);
-				l.to.content = c;
-
-				return l.to;
+			fetch(octokit, config.links[i].to).then((c) => {
+				core.debug(
+					`fetched ${JSON.stringify(config.links[i].to)}: ${JSON.stringify(c)}`,
+				);
+				config.links[i].to.content = c;
 			}),
 		);
 	}
 
-	return Promise.all(promises);
+	return Promise.all(promises).then(() => config);
 }
 
 async function fetch(octokit, { repo: { owner, repo }, path }) {
@@ -38,6 +57,7 @@ async function fetch(octokit, { repo: { owner, repo }, path }) {
 		)
 		.catch((e) => {
 			core.error(`failed to fetch ${owner}/${repo}:${path}: ${e}`);
+			return undefined;
 		});
 }
 
