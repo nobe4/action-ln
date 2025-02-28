19861f1d00fc6dd504f9315715b9b7d7616fa15b
nobe4 <nobe4@users.noreply.github.com>
2025-02-28 11:25:04 +0100

    feat(github): add User request
    This is mostly a test to confirm that I can make requests to the
    GitHub's API correctly.

--- cmd/action-ln/main.go
+++ cmd/action-ln/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/nobe4/action-ln/internal/github"
+)
+
+const (
+	endpoint = "https://api.github.com"
 )
 
 func main() {
-	fmt.Fprintln(os.Stdout, "Hello, actions!")
+	token := os.Getenv("GITHUB_TOKEN")
+	g := github.New(token, endpoint)
+
+	u, err := g.GetUser(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(os.Stdout, "Hello, "+u.Login)
 }
