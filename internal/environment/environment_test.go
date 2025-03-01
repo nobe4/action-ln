cd74ce314564a7bf99b44408a5b4a1bb759f99d8
nobe4 <nobe4@users.noreply.github.com>
2025-03-01 13:32:29 +0100

    test(environment): test parseRepo

--- internal/environment/environment_test.go
+++ internal/environment/environment_test.go
@@ -44,3 +44,37 @@ func TestParseToken(t *testing.T) {
 		}
 	})
 }
+
+//nolint:tparallel // t.Setenv is not thread-safe.
+func TestParseRepo(t *testing.T) {
+	t.Run("gets nothing", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := parseRepo()
+		if !errors.Is(err, errNoRepo) {
+			t.Fatalf("want %v but got error: %v", errNoRepo, err)
+		}
+	})
+
+	t.Run("gets nothing", func(t *testing.T) {
+		t.Setenv("GITHUB_REPOSITORY", "owner+repo+is+invalid")
+
+		_, err := parseRepo()
+		if !errors.Is(err, errInvalidRepo) {
+			t.Fatalf("want %v but got error: %v", errInvalidRepo, err)
+		}
+	})
+
+	t.Run("gets the parsed Repo", func(t *testing.T) {
+		t.Setenv("GITHUB_REPOSITORY", "owner/repo")
+
+		got, err := parseRepo()
+		if err != nil {
+			t.Fatalf("got error: %v", err)
+		}
+
+		if got.Owner.Login != "owner" || got.Repo != "repo" {
+			t.Fatalf("want %v but got %+v", "owner/repo", got)
+		}
+	})
+}
