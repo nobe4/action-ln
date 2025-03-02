558ee0ffed387619d6a2a41e4fcff3ab72465601
nobe4 <nobe4@users.noreply.github.com>
2025-03-02 23:43:19 +0100

    feat(branch): implement and test CreateBranch

--- internal/github/branch.go
+++ internal/github/branch.go
@@ -1,8 +1,11 @@
 package github
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 type Commit struct {
@@ -20,8 +23,37 @@ func (g GitHub) GetBranch(ctx context.Context, repo Repo, branch string) (Branch
 
 	path := fmt.Sprintf("/repos/%s/%s/branches/%s", repo.Owner.Login, repo.Repo, branch)
 
-	if err := g.req(ctx, "GET", path, nil, &b); err != nil {
+	if err := g.req(ctx, http.MethodGet, path, nil, &b); err != nil {
 		return b, fmt.Errorf("failed to get branch: %w", err)
+	}
+
+	return b, nil
+}
+
+// https://docs.github.com/en/rest/git/refs?apiVersion=2022-11-28#create-a-reference
+func (g GitHub) CreateBranch(ctx context.Context, repo Repo, branch, sha string) (Branch, error) {
+	b := Branch{
+		Name: branch,
+		Commit: Commit{
+			SHA: sha,
+		},
+	}
+
+	path := fmt.Sprintf("/repos/%s/%s/git/refs", repo.Owner.Login, repo.Repo)
+
+	body, err := json.Marshal(struct {
+		Ref string `json:"ref"`
+		SHA string `json:"sha"`
+	}{
+		Ref: "refs/heads/" + branch,
+		SHA: sha,
+	})
+	if err != nil {
+		return b, fmt.Errorf("failed to marshal request: %w", err)
+	}
+
+	if err := g.req(ctx, http.MethodPost, path, bytes.NewReader(body), nil); err != nil {
+		return b, fmt.Errorf("failed to create branch: %w", err)
 	}
 
 	return b, nil
