ff1b800b0a0a9b0bdce993ab46fe57fb4daa9ce3
nobe4 <nobe4@users.noreply.github.com>
2025-03-02 23:44:16 +0100

    feat(main): add test to create new branch

--- cmd/action-ln/main.go
+++ cmd/action-ln/main.go
@@ -24,31 +24,39 @@ func main() {
 	g := github.New(e.Token, endpoint)
 	ctx := context.TODO()
 
-	if u, err := g.GetUser(ctx); err != nil {
-		fmt.Fprintln(os.Stderr, "Error getting user:", err)
-	} else {
-		fmt.Fprintln(os.Stdout, "User:", u.Login)
-	}
+	// if u, err := g.GetUser(ctx); err != nil {
+	// 	fmt.Fprintln(os.Stderr, "Error getting user:", err)
+	// } else {
+	// 	fmt.Fprintln(os.Stdout, "User:", u.Login)
+	// }
+	//
+	// if c, err := g.GetContent(
+	// 	ctx,
+	// 	e.Repo,
+	// 	"README.md",
+	// ); err != nil {
+	// 	fmt.Fprintln(os.Stderr, "Error getting contents:", err)
+	// } else {
+	// 	fmt.Fprintln(os.Stdout, "Content:\n", c.Content)
+	// }
 
-	if c, err := g.GetContent(
-		ctx,
-		e.Repo,
-		"README.md",
-	); err != nil {
-		fmt.Fprintln(os.Stderr, "Error getting contents:", err)
-	} else {
-		fmt.Fprintln(os.Stdout, "Content:\n", c.Content)
-	}
+	// if b, err := g.GetDefaultBranch(ctx, e.Repo); err != nil {
+	// 	fmt.Fprintln(os.Stderr, "Error getting default branch:", err)
+	// } else {
+	// 	fmt.Fprintln(os.Stdout, "Default branch:", b)
+	// }
 
-	if b, err := g.GetDefaultBranch(ctx, e.Repo); err != nil {
-		fmt.Fprintln(os.Stderr, "Error getting default branch:", err)
-	} else {
-		fmt.Fprintln(os.Stdout, "Default branch:", b)
-	}
+	var b github.Branch
 
-	if b, err := g.GetBranch(ctx, e.Repo, "main"); err != nil {
-		fmt.Fprintln(os.Stderr, "Error getting branch", "test", err)
+	if b, err = g.GetBranch(ctx, e.Repo, "main"); err != nil {
+		fmt.Fprintln(os.Stderr, "Error getting branch", "main", err)
 	} else {
 		fmt.Fprintln(os.Stdout, "Branch main:", b)
+	}
+
+	if nb, err := g.CreateBranch(ctx, e.Repo, "test", b.Commit.SHA); err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating branch", "test", err)
+	} else {
+		fmt.Fprintln(os.Stdout, "Created branch:", nb)
 	}
 }
