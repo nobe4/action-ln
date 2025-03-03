9a8330e5231c56dfadf70c5651e305d02947e411
nobe4 <nobe4@users.noreply.github.com>
2025-03-03 23:52:07 +0100

    feat(github): return the status code from the req method

--- internal/github/github.go
+++ internal/github/github.go
@@ -42,19 +42,19 @@ type User struct {
 func (g GitHub) GetUser(ctx context.Context) (User, error) {
 	u := User{}
 
-	if err := g.req(ctx, http.MethodGet, PathUser, nil, &u); err != nil {
+	if _, err := g.req(ctx, http.MethodGet, PathUser, nil, &u); err != nil {
 		return u, fmt.Errorf("failed to get user: %w", err)
 	}
 
 	return u, nil
 }
 
-func (g GitHub) req(ctx context.Context, method, path string, body io.Reader, out any) error {
+func (g GitHub) req(ctx context.Context, method, path string, body io.Reader, out any) (int, error) {
 	path = g.endpoint + path
 
 	req, err := http.NewRequestWithContext(ctx, method, path, body)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return http.StatusInternalServerError, fmt.Errorf("failed to create request: %w", err)
 	}
 
 	req.Header.Set("Accept", "application/vnd.github.v3+json")
@@ -62,21 +62,21 @@ func (g GitHub) req(ctx context.Context, method, path string, body io.Reader, ou
 
 	res, err := g.client.Do(req)
 	if err != nil {
-		return fmt.Errorf("%w: %w", errRequest, err)
+		return res.StatusCode, fmt.Errorf("%w: %w", errRequest, err)
 	}
 	defer res.Body.Close()
 
 	// All the 2XX codes
 	success := res.StatusCode >= http.StatusOK && res.StatusCode < http.StatusMultipleChoices
 	if !success {
-		return fmt.Errorf("%w: %s", errRequest, res.Status)
+		return res.StatusCode, fmt.Errorf("%w: %s", errRequest, res.Status)
 	}
 
 	if out != nil {
 		if err := json.NewDecoder(res.Body).Decode(out); err != nil {
-			return fmt.Errorf("failed to decode response: %w", err)
+			return http.StatusInternalServerError, fmt.Errorf("failed to decode response: %w", err)
 		}
 	}
 
-	return nil
+	return res.StatusCode, nil
 }
