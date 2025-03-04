9f98eecdfc77931f8c255ec086a1a6f1d06d842e
nobe4 <nobe4@users.noreply.github.com>
2025-03-04 20:54:48 +0100

    refactor(file): rename content to file
    This is slightly less confusing.

--- internal/github/file.go
+++ internal/github/file.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 )
 
-type Content struct {
+type File struct {
 	Name       string `json:"name"`
 	Path       string `json:"path"`
 	RawContent string `json:"content"`
@@ -18,17 +18,17 @@ type Content struct {
 }
 
 // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content
-func (g GitHub) GetContent(ctx context.Context, repo Repo, path string) (Content, error) {
+func (g GitHub) GetFile(ctx context.Context, repo Repo, path string) (File, error) {
 	path = fmt.Sprintf("/repos/%s/%s/contents/%s", repo.Owner.Login, repo.Repo, path)
 
-	c := Content{}
+	c := File{}
 	if _, err := g.req(ctx, http.MethodGet, path, nil, &c); err != nil {
-		return Content{}, fmt.Errorf("failed to get user: %w", err)
+		return File{}, fmt.Errorf("failed to get user: %w", err)
 	}
 
 	decoded, err := base64.StdEncoding.DecodeString(c.RawContent)
 	if err != nil {
-		return Content{}, fmt.Errorf("failed to decode content: %w", err)
+		return File{}, fmt.Errorf("failed to decode content: %w", err)
 	}
 
 	c.Content = string(decoded)
@@ -37,13 +37,7 @@ func (g GitHub) GetContent(ctx context.Context, repo Repo, path string) (Content
 }
 
 // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents
-func (g GitHub) CreateOrUpdateContent(
-	ctx context.Context,
-	repo Repo,
-	c Content,
-	branch,
-	message string,
-) (Content, error) {
+func (g GitHub) UpdateFile(ctx context.Context, repo Repo, f File, branch, message string) (File, error) {
 	body, err := json.Marshal(struct {
 		Message string `json:"message"`
 		Content string `json:"content"`
@@ -51,32 +45,32 @@ func (g GitHub) CreateOrUpdateContent(
 		Branch  string `json:"branch"`
 	}{
 		Message: message,
-		Content: base64.StdEncoding.EncodeToString([]byte(c.Content)),
+		Content: base64.StdEncoding.EncodeToString([]byte(f.Content)),
 		Branch:  branch,
-		SHA:     c.SHA,
+		SHA:     f.SHA,
 	})
 	if err != nil {
-		return Content{}, fmt.Errorf("failed to marshal request: %w", err)
+		return File{}, fmt.Errorf("failed to marshal request: %w", err)
 	}
 
 	// NOTE: The response wrapes the content in an extra `{ "content": {} }`.
-	// It's not technically the same as the Content we get from GetContent. For
+	// It's not technically the same as the File we get from GetFile. For
 	// this purpose it's enough, we can reinject the `Content` value after.
 	out := struct {
-		Content Content `json:"content"`
+		File File `json:"content"`
 	}{}
 
 	if _, err := g.req(
 		ctx,
 		http.MethodPut,
-		fmt.Sprintf("/repos/%s/%s/contents/%s", repo.Owner.Login, repo.Repo, c.Path),
+		fmt.Sprintf("/repos/%s/%s/contents/%s", repo.Owner.Login, repo.Repo, f.Path),
 		bytes.NewReader(body),
 		&out,
 	); err != nil {
-		return Content{}, fmt.Errorf("failed to create branch: %w", err)
+		return File{}, fmt.Errorf("failed to create branch: %w", err)
 	}
 
-	out.Content.Content = c.Content
+	out.File.Content = f.Content
 
-	return out.Content, nil
+	return out.File, nil
 }
