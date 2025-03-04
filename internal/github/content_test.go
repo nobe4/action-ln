adf11d0b7c24f42baac374af7f7a50eba34f1af6
nobe4 <nobe4@users.noreply.github.com>
2025-03-04 20:47:33 +0100

    test(content): write tests

--- internal/github/content_test.go
+++ internal/github/content_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -53,6 +54,73 @@ func TestGetContent(t *testing.T) {
 
 		if c.Content != "ok" {
 			t.Fatalf("expected content to be 'ok' but got %s", c.Content)
+		}
+	})
+}
+
+func TestCreateOrUpdateContent(t *testing.T) {
+	t.Parallel()
+
+	repo := Repo{Owner: User{"owner"}, Repo: "repo"}
+	content := Content{
+		Content: "ok",
+		SHA:     "sha",
+		Path:    "path/to/file",
+	}
+
+	t.Run("fails", func(t *testing.T) {
+		t.Parallel()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+
+		g := New("token", ts.URL)
+
+		_, err := g.CreateOrUpdateContent(t.Context(), repo, content, "branch", "message")
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+	})
+
+	t.Run("succeeds", func(t *testing.T) {
+		t.Parallel()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/repos/owner/repo/contents/path/to/file" {
+				t.Fatal("invalid path", r.URL.Path)
+			}
+
+			if r.Method != http.MethodPut {
+				t.Fatal("invalid method", r.Method)
+			}
+
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				t.Fatal("failed to read body", err)
+			}
+
+			if string(body) != `{"message":"message","content":"b2s=","sha":"sha","branch":"branch"}` {
+				t.Fatal("invalid body", string(body))
+			}
+
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprintln(w, `{"content": {"sha":"newSha"}}`)
+		}))
+
+		g := New("token", ts.URL)
+
+		c, err := g.CreateOrUpdateContent(t.Context(), repo, content, "branch", "message")
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if c.SHA != "newSha" {
+			t.Fatalf("expected new sha to be 'newSha' but got '%s'", c.SHA)
+		}
+
+		if c.Content != content.Content {
+			t.Fatalf("expected content to be '%s' but got '%s'", content.Content, c.Content)
 		}
 	})
 }
