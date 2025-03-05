8805dd97337a9a0662217d15f99eaa87b6502133
nobe4 <nobe4@users.noreply.github.com>
2025-03-05 19:11:38 +0100

    feat(pull): implement and test GetOrCreatePull

--- internal/github/pull_test.go
+++ internal/github/pull_test.go
@@ -8,35 +8,44 @@ import (
 	"testing"
 )
 
+const (
+	head         = "head"
+	base         = "base"
+	title        = "title"
+	body         = "body"
+	number       = 123
+	pullAPIPath  = "/repos/owner/repo/pulls"
+	pullAPIQuery = "base=base&head=owner%3Ahead&per_page=1&state=open"
+)
+
+var repo = Repo{Owner: User{Login: "owner"}, Repo: "repo"}
+
 func TestGetPull(t *testing.T) {
 	t.Parallel()
-
-	repo := Repo{Owner: User{Login: "owner"}, Repo: "repo"}
 
 	t.Run("finds a pull", func(t *testing.T) {
 		t.Parallel()
 
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			assertReq(t, r, http.MethodGet, "/repos/owner/repo/pulls", nil)
+			assertReq(t, r, http.MethodGet, pullAPIPath, nil)
 
-			expectedQuery := "base=base&head=owner%3Ahead&per_page=1&state=open"
-			if r.URL.RawQuery != expectedQuery {
-				t.Fatalf("expected query to be '%s' but got '%s'", expectedQuery, r.URL.RawQuery)
+			if r.URL.RawQuery != pullAPIQuery {
+				t.Fatalf("expected query to be '%s' but got '%s'", pullAPIQuery, r.URL.RawQuery)
 			}
 
 			w.WriteHeader(http.StatusOK)
-			fmt.Fprintln(w, `[{"number": 123}]`)
+			fmt.Fprintf(w, `[{"number": %d}]\n`, number)
 		}))
 
 		g := New("token", ts.URL)
 
-		got, err := g.GetPull(t.Context(), repo, "base", "head")
+		got, err := g.GetPull(t.Context(), repo, base, head)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
 
-		if got.Number != 123 {
-			t.Fatalf("expected number to be '123' but got %d", got.Number)
+		if got.Number != number {
+			t.Fatalf("expected number to be %d but got %d", number, got.Number)
 		}
 	})
 
@@ -83,23 +92,97 @@ func TestCreatePull(t *testing.T) {
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			assertReq(t, r,
 				http.MethodPost,
-				"/repos/owner/repo/pulls",
+				pullAPIPath,
 				[]byte(`{"title":"title","head":"owner:head","base":"base","body":"body"}`),
 			)
 
 			w.WriteHeader(http.StatusCreated)
-			fmt.Fprintln(w, `{"number": 123}`)
+			fmt.Fprintf(w, `{"number": %d}\n`, number)
 		}))
 
 		g := New("token", ts.URL)
 
-		got, err := g.CreatePull(t.Context(), repo, "base", "head", "title", "body")
+		got, err := g.CreatePull(t.Context(), repo, base, head, title, body)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
 
-		if got.Number != 123 {
-			t.Fatalf("expected number to be '123' but got %d", got.Number)
+		if got.Number != number {
+			t.Fatalf("expected number to be %d but got %d", number, got.Number)
+		}
+	})
+}
+
+func TestGetOrCreatePull(t *testing.T) {
+	t.Parallel()
+
+	t.Run("finds existing pull", func(t *testing.T) {
+		t.Parallel()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.RawQuery != pullAPIQuery {
+				t.Fatalf("expected query to be '%s' but got '%s'", pullAPIQuery, r.URL.RawQuery)
+			}
+
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, `[{"number": %d}]\n`, number)
+		}))
+
+		g := New("token", ts.URL)
+
+		got, err := g.GetOrCreatePull(t.Context(), repo, base, head, title, body)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if got.Number != number {
+			t.Fatalf("want number %d, but got %d", number, got.Number)
+		}
+	})
+
+	t.Run("fails to get existing pull", func(t *testing.T) {
+		t.Parallel()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+
+		g := New("token", ts.URL)
+
+		_, err := g.GetOrCreatePull(t.Context(), repo, base, head, title, body)
+		if err == nil {
+			t.Fatalf("expected error, got nil")
+		}
+	})
+
+	t.Run("creates pull if it does not exist", func(t *testing.T) {
+		t.Parallel()
+
+		reqIndex := 0
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch reqIndex {
+			case 0:
+				assertReq(t, r, http.MethodGet, pullAPIPath, nil)
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintln(w, `[]`)
+			case 1:
+				assertReq(t, r, http.MethodPost, pullAPIPath, nil)
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintf(w, `{"number": %d}\n`, number)
+			}
+
+			reqIndex++
+		}))
+
+		g := New("token", ts.URL)
+
+		got, err := g.GetOrCreatePull(t.Context(), repo, base, head, title, body)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if got.Number != number {
+			t.Fatalf("expected number to be %d but got %d", number, got.Number)
 		}
 	})
 }
