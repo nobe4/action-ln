82fbab211a6994fc68109f17bd76ebdd21601ea7
nobe4 <nobe4@users.noreply.github.com>
2025-03-09 21:23:09 +0100

    refactor(file): use github.File only
    Deprecate config.File

--- internal/config/file.go
+++ internal/config/file.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+
+	"github.com/nobe4/action-ln/internal/github"
 )
 
 var (
@@ -12,18 +14,7 @@ var (
 	ErrInvalidFileFormat = errors.New("invalid file format")
 )
 
-type File struct {
-	Repo  string `yaml:"repo"`
-	Owner string `yaml:"owner"`
-	Path  string `yaml:"path"`
-	Ref   string `yaml:"ref"`
-}
-
-func (f File) Equal(o File) bool {
-	return f.Repo == o.Repo && f.Owner == o.Owner && f.Path == o.Path
-}
-
-func parseFile(rawFile any) (File, error) {
+func parseFile(rawFile any) (github.File, error) {
 	switch v := rawFile.(type) {
 	case map[string]any:
 		return parseFileMap(v)
@@ -31,68 +22,93 @@ func parseFile(rawFile any) (File, error) {
 		return parseFileString(v)
 
 	default:
-		return File{}, fmt.Errorf("%w: %v (%T)", ErrInvalidFileType, rawFile, rawFile)
+		return github.File{}, fmt.Errorf("%w: %v (%T)", ErrInvalidFileType, rawFile, rawFile)
 	}
 }
 
-func parseFileMap(rawFile map[string]any) (File, error) {
-	f := File{}
+func parseFileMap(rawFile map[string]any) (github.File, error) {
+	f := github.File{}
 
-	f.Repo = getMapKey(rawFile, "repo")
-	f.Owner = getMapKey(rawFile, "owner")
+	f.Repo = github.Repo{
+		Owner: github.User{
+			Login: getMapKey(rawFile, "owner"),
+		},
+		Repo: getMapKey(rawFile, "repo"),
+	}
 	f.Path = getMapKey(rawFile, "path")
 	f.Ref = getMapKey(rawFile, "ref")
 
-	if strings.Contains(f.Repo, "/") {
-		parts := strings.Split(f.Repo, "/")
+	if strings.Contains(f.Repo.Repo, "/") {
+		parts := strings.Split(f.Repo.Repo, "/")
 		if len(parts) != 2 { //nolint:all // TODO: log that the repo is badly formatted
 			// don't do anything
 		}
 
-		f.Owner = parts[0]
-		f.Repo = parts[1]
+		f.Repo.Owner.Login = parts[0]
+		f.Repo.Repo = parts[1]
 	}
 
 	return f, nil
 }
 
-func parseFileString(s string) (File, error) {
+func parseFileString(s string) (github.File, error) {
 	// 'https://github.com/owner/repo/blob/ref/path/to/file'
 	if m := regexp.
 		MustCompile(`^https://github.com/(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/blob/(?P<ref>[\w-]+)/(?P<path>.+)$`).
 		FindStringSubmatch(s); len(m) > 0 {
-		return File{Owner: m[1], Repo: m[2], Ref: m[3], Path: m[4]}, nil
+		return github.File{
+			Repo: github.Repo{
+				Owner: github.User{Login: m[1]},
+				Repo:  m[2],
+			},
+			Ref:  m[3],
+			Path: m[4],
+		}, nil
 	}
 
 	// 'owner/repo/blob/ref/path/to/file'
 	if m := regexp.
 		MustCompile(`^(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/blob/(?P<ref>[\w-]+)/(?P<path>.+)$`).
 		FindStringSubmatch(s); len(m) > 0 {
-		return File{Owner: m[1], Repo: m[2], Ref: m[3], Path: m[4]}, nil
+		return github.File{
+			Repo: github.Repo{
+				Owner: github.User{Login: m[1]},
+				Repo:  m[2],
+			},
+			Ref:  m[3],
+			Path: m[4],
+		}, nil
 	}
 
 	// 'owner/repo:path/to/file@ref'
 	if m := regexp.
 		MustCompile(`^(?P<owner>[\w-]+)/(?P<repo>[\w-]+):(?P<path>.+)@(?P<ref>[\w-]+)$`).
 		FindStringSubmatch(s); len(m) > 0 {
-		return File{Owner: m[1], Repo: m[2], Path: m[3], Ref: m[4]}, nil
+		return github.File{
+			Repo: github.Repo{
+				Owner: github.User{Login: m[1]},
+				Repo:  m[2],
+			},
+			Ref:  m[3],
+			Path: m[4],
+		}, nil
 	}
 
 	// 'path/to/file@ref'
 	if m := regexp.
 		MustCompile(`^(?P<path>[^@]+)@(?P<ref>[\w-]+)$`).
 		FindStringSubmatch(s); len(m) > 0 {
-		return File{Path: m[1], Ref: m[2]}, nil
+		return github.File{Path: m[1], Ref: m[2]}, nil
 	}
 
 	// 'path/to/file'
 	if m := regexp.
 		MustCompile(`^(?P<path>.+)$`).
 		FindStringSubmatch(s); len(m) > 0 {
-		return File{Path: m[1]}, nil
+		return github.File{Path: m[1]}, nil
 	}
 
-	return File{}, fmt.Errorf("%w: %v", ErrInvalidFileFormat, s)
+	return github.File{}, fmt.Errorf("%w: %v", ErrInvalidFileFormat, s)
 }
 
 func getMapKey(m map[string]any, k string) string {
