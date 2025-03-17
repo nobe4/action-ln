54db35e1ef6f01a6cb22359233f6b989e7f4fc3d
nobe4 <nobe4@users.noreply.github.com>
2025-03-17 11:26:08 +0100

    feat(main): test the logger

--- cmd/action-ln/main.go
+++ cmd/action-ln/main.go
@@ -1,40 +1,53 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log/slog"
 	"os"
+	"time"
 
-	"github.com/nobe4/action-ln/internal/environment"
-	"github.com/nobe4/action-ln/internal/github"
-	"github.com/nobe4/action-ln/internal/ln"
+	"github.com/nobe4/action-ln/internal/log"
 )
 
 func main() {
-	ctx := context.TODO()
+	debug := true
 
-	e, err := environment.Parse()
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error parsing environment\n%v\n", err)
-		os.Exit(1)
-	}
+	handler := log.NewGitHubHandler(os.Stdout, debug)
+	slog.SetDefault(slog.New(handler))
 
-	fmt.Fprintln(os.Stdout, "Environment:", e)
-
-	g := github.New(e.Endpoint)
-
-	if err = g.Auth(ctx,
-		e.Token,
-		e.App.ID,
-		e.App.PrivateKey,
-		e.App.InstallID,
-	); err != nil {
-		fmt.Fprintf(os.Stdout, "Error authenticating\n%v\n", err)
-		os.Exit(1)
-	}
-
-	if err := ln.Run(ctx, e, g); err != nil {
-		fmt.Fprintf(os.Stdout, "Error running action-ln\n%v\n", err)
-		os.Exit(1)
-	}
+	log.Group("test")
+	log.Info("message", "a", 1)
+	log.Debug("message")
+	log.Error("message", "a", []string{"x", "y", "z"})
+	log.Warn("message")
+	log.Notice("message", "a", time.Now())
+	log.GroupEnd()
 }
+
+// func main() {
+// 	ctx := context.TODO()
+//
+// 	e, err := environment.Parse()
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stdout, "Error parsing environment\n%v\n", err)
+// 		os.Exit(1)
+// 	}
+//
+// 	fmt.Fprintln(os.Stdout, "Environment:", e)
+//
+// 	g := github.New(e.Endpoint)
+//
+// 	if err = g.Auth(ctx,
+// 		e.Token,
+// 		e.App.ID,
+// 		e.App.PrivateKey,
+// 		e.App.InstallID,
+// 	); err != nil {
+// 		fmt.Fprintf(os.Stdout, "Error authenticating\n%v\n", err)
+// 		os.Exit(1)
+// 	}
+//
+// 	if err := ln.Run(ctx, e, g); err != nil {
+// 		fmt.Fprintf(os.Stdout, "Error running action-ln\n%v\n", err)
+// 		os.Exit(1)
+// 	}
+// }
