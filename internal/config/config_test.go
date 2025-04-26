4cebd84260d52901854b0a41f5c5ac30da75468b
nobe4 <nobe4@users.noreply.github.com>
2025-04-26 16:38:42 +0200

    feat(config): add a simpler way to document, parse, test the links
    cc #66
    cc #82

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	_ "embed"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -8,6 +10,38 @@ import (
 
 	"github.com/nobe4/action-ln/internal/github"
 )
+
+//go:embed all-cases.yaml
+var allCases string
+
+func TestConfigParseAll(t *testing.T) {
+	t.Parallel()
+
+	c := New()
+
+	err := c.Parse(strings.NewReader(allCases))
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	wants := []string{}
+
+	for _, want := range regexp.
+		MustCompile(`# want: (.+)`).
+		FindAllStringSubmatch(allCases, -1) {
+		wants = append(wants, want[1])
+	}
+
+	if ll, lw := len(c.Links), len(wants); ll != lw {
+		t.Fatalf("want %d links, but got %d", lw, ll)
+	}
+
+	for i, l := range c.Links {
+		if l.String() != wants[i] {
+			t.Fatalf("want link %d to be %q, but got %q", i, wants[i], l.String())
+		}
+	}
+}
 
 func TestConfigParse(t *testing.T) {
 	t.Parallel()
