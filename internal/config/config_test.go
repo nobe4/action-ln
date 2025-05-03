4a53ba8f3983e694d071b03d34bb57a5f3295823
nobe4 <nobe4@users.noreply.github.com>
2025-05-03 10:21:30 +0200

    test(config): add a no-defaults case

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,44 +7,60 @@ import (
 	"testing"
 )
 
-//go:embed all-cases.yaml
+//go:embed fixtures/all-cases.yaml
 var allCases string
+
+//go:embed fixtures/all-cases-no-defaults.yaml
+var allCasesNoDefaults string
 
 func TestConfigParseAll(t *testing.T) {
 	t.Parallel()
 
-	c := New()
+	test := func(t *testing.T, path, content string) {
+		t.Helper()
 
-	err := c.Parse(strings.NewReader(allCases))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+		t.Run(path, func(t *testing.T) {
+			t.Parallel()
+
+			c := New()
+
+			t.Log(content)
+
+			err := c.Parse(strings.NewReader(content))
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			for i, l := range c.Links {
+				t.Logf("LINK[%d] %s", i, l.String())
+			}
+
+			wants := []string{}
+
+			for _, want := range regexp.
+				MustCompile(`(?m)^\s+# want: (.+)$`).
+				FindAllStringSubmatch(content, -1) {
+				wants = append(wants, want[1])
+			}
+
+			for i, want := range wants {
+				t.Logf("WANT[%d] %s", i, want)
+			}
+
+			if ll, lw := len(c.Links), len(wants); ll != lw {
+				t.Fatalf("want %d links, but got %d", lw, ll)
+			}
+
+			for i, l := range c.Links {
+				if l.String() != wants[i] {
+					t.Fatalf("want link %d to be %q, but got %q", i, wants[i], l.String())
+				}
+			}
+		})
 	}
 
-	for i, l := range c.Links {
-		t.Logf("LINK[%d] %s", i, l.String())
-	}
-
-	wants := []string{}
-
-	for _, want := range regexp.
-		MustCompile(`(?m)^\s+# want: (.+)$`).
-		FindAllStringSubmatch(allCases, -1) {
-		wants = append(wants, want[1])
-	}
-
-	for i, want := range wants {
-		t.Logf("WANT[%d] %s", i, want)
-	}
-
-	if ll, lw := len(c.Links), len(wants); ll != lw {
-		t.Fatalf("want %d links, but got %d", lw, ll)
-	}
-
-	for i, l := range c.Links {
-		if l.String() != wants[i] {
-			t.Fatalf("want link %d to be %q, but got %q", i, wants[i], l.String())
-		}
-	}
+	test(t, "all-cases.yaml", allCases)
+	test(t, "all-cases-no-defaults.yaml", allCasesNoDefaults)
 }
 
 func TestGetMapKey(t *testing.T) {
