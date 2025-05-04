4b13762b9b665634d0ea9185174f60919fb3a241
nobe4 <nobe4@users.noreply.github.com>
2025-05-04 09:08:14 +0200

    chore(dist): prepare v0.0.17 release

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const childProcess = require("child_process");
 const os = require("os");
 const process = require("process");
 
-const version = "dev";
+const version = "v0.0.17";
 
 const platform = os.platform();
 const arch = {
