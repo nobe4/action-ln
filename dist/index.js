419cc628e709824c6eea734bf6f6d0e0095ef762
nobe4 <nobe4@users.noreply.github.com>
2025-08-05 15:49:52 +0200

    chore(version): bump
    This is mostly to ensure that the `gh-ln` import in the previous PR
    worked correctly.

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const childProcess = require("child_process");
 const os = require("os");
 const process = require("process");
 
-const version = "v0.1.0";
+const version = "dev";
 
 const platform = os.platform();
 const arch = {
